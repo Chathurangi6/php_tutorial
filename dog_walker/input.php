8c63f6c2991fd58ce1bb0884182cbfac6a6982a9
Sachini Chathurangi <chathurangi666@gmail.com>
2018-10-17 23:45:46 -0700

    added walkers file

---
+++ dog_walker/input.php
@@ -0,0 +1,13 @@
+<html>
+		<head></head>
+		<body>
+		
+			<form method="post" action="calculate.php">
+			Number of Walkers : <input name="walkers" type="text"><br>
+			Number of Dogs : <input name="dogs" type="text"><br>
+			Sizes of Dogs(seperate with spaces in ascending order) : <input name="sizes" type="text"><br>
+			<input type="submit" name="submit" value="Calculate">
+			</form>
+			
+		</body>
+	</html>
